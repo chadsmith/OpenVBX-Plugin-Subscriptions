1b38467886ba47e493c794dbf73492b1f7624989
Chad Smith <chad@nospam.me>
2010-06-28 21:53:26 -0500

    Added dispatch applet to allow for group text / sending updates via text message

---
+++ applets/dispatch/twiml.php
@@ -0,0 +1,48 @@
+<?php
+$ci =& get_instance();
+$dispatcher = AppletInstance::getUserGroupPickerValue('dispatcher');
+$list = AppletInstance::getValue('list');
+$sender = normalize_phone_to_E164($_REQUEST['From']);
+$number = normalize_phone_to_E164($_REQUEST['To']);
+$body = $_REQUEST['Body'];
+$dispatch = false;
+
+if(is_null($dispatcher))
+	$dispatch = true;
+else
+	switch(get_class($dispatcher)){
+		case 'VBX_User':
+			foreach($dispatcher->devices as $device)
+				if($sender == $device->value)
+					$dispatch = true;
+			break;
+		case 'VBX_Group':
+			foreach($moderator->users as $user){
+				$user = VBX_User::get($user->user_id);
+				foreach($user->devices as $device)
+					if($sender == $device->value)
+						$dispatch = true;
+			}
+		break;
+	}
+
+$response = new Response();
+
+if($dispatch){
+	$subscribers = $ci->db->query(sprintf('SELECT value FROM subscribers WHERE list=%d',$list))->result();
+	require_once(APPPATH . 'libraries/twilio.php');
+	$ci->twilio = new TwilioRestClient($ci->twilio_sid, $ci->twilio_token, $ci->twilio_endpoint);
+	if($body&&count($subscribers))
+		foreach($subscribers as $subscriber)
+			$ci->twilio->request("Accounts/{$ci->twilio_sid}/SMS/Messages", 'POST', array('To' => $subscriber->value, 'From' => $number, 'Body' => $body));
+	$dispatched = AppletInstance::getDropZoneUrl('dispatched');
+	if(!empty($dispatched))
+		$response->addRedirect($dispatched);
+}
+else{
+	$next = AppletInstance::getDropZoneUrl('next');
+	if(!empty($next))
+		$response->addRedirect($next);
+}
+
+$response->Respond();
---
+++ applets/dispatch/ui.php
@@ -0,0 +1,39 @@
+<?php
+	$user = OpenVBX::getCurrentUser();
+	$tenant_id = $user->values['tenant_id'];
+	$ci =& get_instance();
+	$selected = AppletInstance::getValue('list');
+	$action = AppletInstance::getValue('action');
+	$queries = explode(';', file_get_contents(dirname(dirname(dirname(__FILE__))).'/db.sql'));
+	foreach($queries as $query)
+		if(trim($query))
+			$ci->db->query($query);
+	$lists = $ci->db->query(sprintf('SELECT id, name FROM subscribers_lists WHERE tenant=%d', $tenant_id))->result();
+?>
+<div class="vbx-applet">
+<?php if(count($lists)): ?>
+	<div class="vbx-full-pane">
+		<h2>Dispatcher</h2>
+		<p>If the message is from this group or user:</p>
+<?php echo AppletUI::UserGroupPicker('dispatcher'); ?>
+		<h3>Dispatch it to: </h3>
+		<fieldset class="vbx-input-container">
+			<select class="medium" name="list">
+<?php foreach($lists as $list): ?>
+				<option value="<?php echo $list->id; ?>"<?php echo $list->id==$selected?' selected="selected" ':''; ?>><?php echo $list->name; ?></option>
+<?php endforeach; ?>
+			</select>
+		</fieldset>
+		<h3>And then:</h3>
+<?php echo AppletUI::DropZone('dispatched'); ?>
+	</div>
+	<div class="vbx-full-pane">
+		<h2>Otherwise:</h2>
+<?php echo AppletUI::DropZone('next'); ?>
+	</div>
+<?php else: ?>
+	<div class="vbx-full-pane">
+		<h3>You need to create a subscription list first.</h3>
+	</div>
+<?php endif; ?>
+</div><!-- .vbx-applet -->
