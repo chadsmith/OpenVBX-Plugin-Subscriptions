43b6d756f57c8ef705cb6c78409a5b319b97614e
Chad Smith <chad@nospam.me>
2011-01-18 10:34:24 -0600

    Added "If Member" plugin. Redirect flows based on list membership.

---
+++ applets/if/twiml.php
@@ -0,0 +1,13 @@
+<?php
+$ci =& get_instance();
+$list = AppletInstance::getValue('list');
+$number = 'voice' == AppletInstance::getFlowType() ? normalize_phone_to_E164($_REQUEST['Caller']) : normalize_phone_to_E164($_REQUEST['From']);
+$subscriber = $ci->db->query(sprintf('SELECT id FROM subscribers WHERE list=%d AND value=%s', $list, $number))->num_rows() > 0;
+
+$response = new Response();
+
+$next = AppletInstance::getDropZoneUrl($subscriber ? 'pass' : 'fail');
+if(!empty($next))
+	$response->addRedirect($next);
+
+$response->Respond();
