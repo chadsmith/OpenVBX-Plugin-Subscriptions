09ca711f28f3223c08f8a2e8d46686aa1017d739
Chad Smith <chad@nospam.me>
2013-01-08 16:21:15 -0600

    Fix for inbound messages

--- applets/if/twiml.php
+++ applets/if/twiml.php
@@ -1,10 +1,9 @@
 <?php
 $ci =& get_instance();
 $list = AppletInstance::getValue('list');
-$direction = 'inbound';
+$direction = isset($_REQUEST['Direction']) ? $_REQUEST['Direction'] : 'inbound';
 
-if(!empty($_REQUEST['Direction'])) {
-  $direction = $_REQUEST['Direction'];
+if(!empty($_REQUEST['From'])) {
 	$number = normalize_phone_to_E164(in_array($direction, array('inbound', 'incoming')) ? $_REQUEST['From'] : $_REQUEST['To']);
 	$subscriber = $ci->db->query(sprintf('SELECT id FROM subscribers WHERE list = %d AND value = %s', $list, $number))->num_rows() > 0;
 	$next = AppletInstance::getDropZoneUrl($subscriber ? 'pass' : 'fail');
--- applets/subscription/twiml.php
+++ applets/subscription/twiml.php
@@ -2,10 +2,9 @@
 $ci =& get_instance();
 $list = AppletInstance::getValue('list');
 $action = AppletInstance::getValue('action');
-$direction = 'inbound';
+$direction = isset($_REQUEST['Direction']) ? $_REQUEST['Direction'] : 'inbound';
 
-if(!empty($_REQUEST['Direction'])) {
-  $direction = $_REQUEST['Direction'];
+if(!empty($_REQUEST['From'])) {
 	$number = normalize_phone_to_E164(in_array($direction, array('inbound', 'incoming')) ? $_REQUEST['From'] : $_REQUEST['To']);
 	if('add' == $action) {
 		if(!$ci->db->query(sprintf('SELECT id FROM subscribers WHERE list = %d AND value = %s', $list, $number))->num_rows())
